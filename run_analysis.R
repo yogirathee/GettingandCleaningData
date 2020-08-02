852416be61cce3195b5748a6c54e4039a6a0435c
Yogender Rathee <yorat@vestas.com>
2020-08-02 11:56:37 +0530

    README updated and code added

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+
+#Data downloaded in local folder. 
+#All files were placed in C:/Users/YORAT/OneDrive - Vestas Wind Systems A S/Desktop/practice/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset
+
+
+setwd("C:/Users/YORAT/OneDrive - Vestas Wind Systems A S/Desktop/practice/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset")
+
+features <- read.table("features.txt", col.names = c("n","functions"))
+activities <- read.table("activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("subject_test.txt", col.names = "subject")
+x_test <- read.table("X_test.txt", col.names = features$functions)
+y_test <- read.table("y_test.txt", col.names = "code")
+subject_train <- read.table("subject_train.txt", col.names = "subject")
+x_train <- read.table("X_train.txt", col.names = features$functions)
+y_train <- read.table("y_train.txt", col.names = "code")
+
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+TidyData$code <- activities[TidyData$code, 2]
+
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
+
+str(FinalData)
